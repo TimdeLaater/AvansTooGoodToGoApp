2fd844a6ed393c16a6d2d3cd0f93e460189b3ccd
TimdeLaater <tdelaater@gmail.com>
2022-10-24 13:24:51 +0200

    Added Package controller

---
+++ AvansToGoodToGo/Controllers/PackageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvansTooGoodToGo.Controllers
+{
+    public class PackageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
